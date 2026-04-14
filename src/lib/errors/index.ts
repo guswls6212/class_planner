54a581efe906b25ec7a4a66c9c5736aa451c277e
guswls6212 <guswls6212@gmail.com>
2026-04-15 01:59:03 +0900

    feat(errors): add barrel export for src/lib/errors

---
+++ src/lib/errors/index.ts
@@ -0,0 +1,5 @@
+// src/lib/errors/index.ts
+export { AppError } from "./AppError";
+export { ErrorCodes, type ErrorCode } from "./codes";
+export { getKoMessage } from "./messages.ko";
+export { toErrorResponse } from "./httpErrors";
